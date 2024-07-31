109c86f6ba7dc2a124d4a53bd072b812774350ca
kaolin223 <m-kuptsov@inbox.ru>
2024-07-31 13:46:02 +0300

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+  const arr = [...n.toString()];
+  const [minus, ...number] = arr;
+  if (minus === '-') {
+    arr.shift();
+  }
+  return Number(arr.reverse().join(''));
+};
